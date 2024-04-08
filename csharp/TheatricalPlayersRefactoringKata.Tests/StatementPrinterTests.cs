4f12a224d7483fce6aec1b7004617cd23f64542b
Tomas Zouhar <xzouha13@vutbr.cz>
2024-04-08 12:41:03 +0200

    fdfs

--- csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -27,6 +27,24 @@ namespace TheatricalPlayersRefactoringKata.Tests
             Approvals.Verify(result);
         }
         [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        public void test_html_statement()
+        {
+            var plays = new Dictionary<string, Play>();
+            plays.Add("hamlet", new Play("Hamlet", "tragedy"));
+            plays.Add("as-like", new Play("As You Like It", "comedy"));
+            plays.Add("othello", new Play("Othello", "tragedy"));
+
+            Invoice invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)});
+            
+            StatementPrinter statementPrinter = new StatementPrinter();
+            var result = statementPrinter.Print(invoice, plays);
+
+            Approvals.Verify(result);
+        }
+        [Fact]
         public void test_statement_with_new_play_types()
         {
             var plays = new Dictionary<string, Play>();
--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -10,8 +10,9 @@ namespace TheatricalPlayersRefactoringKata
         {
             var totalAmount = 0;
             var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
+            var result = string.Format("<h1>Statement for {0}</h1>\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
+            result += "<table>\n";
 
             foreach(var perf in invoice.Performances) 
             {
@@ -46,6 +47,60 @@ namespace TheatricalPlayersRefactoringKata
             }
             result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += String.Format("You earned {0} credits\n", volumeCredits);
+            return result;
+        }
+        
+        public string PrintHTML(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            var totalAmount = 0;
+            var volumeCredits = 0;
+            var result = "";
+            result += "<html>\n";
+            result += "<body>\n";
+            result += string.Format("Statement for {0}\n", invoice.Customer);
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            
+            result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+
+            foreach(var perf in invoice.Performances) 
+            {
+                var play = plays[perf.PlayID];
+                result += $"<tr><td>{play.Name}</td><td>{perf.Audience}</td><td>";
+
+                var thisAmount = 0;
+                switch (play.Type) 
+                {
+                    case "tragedy":
+                        thisAmount = 40000;
+                        if (perf.Audience > 30) {
+                            thisAmount += 1000 * (perf.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        thisAmount = 30000;
+                        if (perf.Audience > 20) {
+                            thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        }
+                        thisAmount += 300 * perf.Audience;
+                        break;
+                    default:
+                        throw new Exception("unknown type: " + play.Type);
+                }
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+                // print line for this order
+                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                totalAmount += thisAmount;
+                result += $"${totalAmount}</td></tr>";
+            }
+            result += "</table>\n";
+            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+            result += String.Format("You earned {0} credits\n", volumeCredits);
+            result += "</body>\n";
+            result += "</html>\n";
             return result;
         }
     }
